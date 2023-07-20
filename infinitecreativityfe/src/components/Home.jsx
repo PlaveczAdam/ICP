5dfd48ef97c585e04a3e8bff9d6e57697dd10fac
Adam Plavecz <plaveczadam79@gmail.com>
2023-07-20 21:12:05 +0200

    add home page

---
+++ infinitecreativityfe/src/components/Home.jsx
@@ -0,0 +1,51 @@
+import { Box } from '@mui/material';
+import { Typography } from '@mui/material/Typography';
+
+function Home(props) {
+
+  return (
+    <Box typography="body1" textAlign="left" padding={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+      dictum ante, ac molestie tellus. Mauris nunc dolor, elementum sed sodales
+      nec, hendrerit nec est. Donec nulla enim, efficitur non lorem nec, euismod
+      venenatis ligula. Aenean nec faucibus ipsum. Vivamus nec tempus neque.
+      Donec mattis pharetra nisi a laoreet. Nam nec magna ac urna pellentesque
+      hendrerit. Ut ac elit eget quam cursus hendrerit ut ac leo. Nullam nec
+      euismod libero. Orci varius natoque penatibus et magnis dis parturient
+      montes, nascetur ridiculus mus. Fusce aliquam dignissim metus, condimentum
+      accumsan tellus porta eu. Fusce finibus convallis suscipit. Nulla luctus
+      lobortis est quis consequat. Integer id erat massa. Sed in nunc eu orci
+      aliquet scelerisque. Nunc tempor blandit libero, nec facilisis lacus
+      eleifend semper. Phasellus molestie, ex eu tincidunt laoreet, magna velit
+      congue libero, id vulputate purus erat nec ipsum. Ut venenatis arcu non
+      dolor bibendum sagittis sed vel diam. Curabitur vitae sapien eu urna
+      vulputate porta. Nullam at dolor non lacus lobortis malesuada. Aliquam
+      venenatis urna neque, nec commodo ligula laoreet a. Maecenas ullamcorper,
+      neque sit amet porta commodo, erat justo condimentum ipsum, in laoreet
+      velit tellus in elit. Nam nec accumsan mauris. Integer congue nisi at
+      dignissim dignissim. Duis suscipit consectetur ex a maximus. Etiam at odio
+      ac ex tincidunt imperdiet eu id quam. Nulla facilisi. Class aptent taciti
+      sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+      Quisque feugiat ultricies congue. Suspendisse vel nisl ut leo malesuada
+      luctus. Class aptent taciti sociosqu ad litora torquent per conubia
+      nostra, per inceptos himenaeos. Donec molestie nisl sit amet lacus dapibus
+      sodales. Praesent mattis ex id justo faucibus, vitae mattis justo rutrum.
+      Maecenas at mi tincidunt, accumsan dui eget, consequat enim. Cras tempus,
+      risus efficitur sagittis molestie, mi odio suscipit dui, in lobortis
+      lectus lorem quis tellus. Fusce posuere, turpis eget congue consectetur,
+      lacus purus lacinia diam, ut malesuada neque dolor et diam. Maecenas
+      facilisis lacinia nibh, eget imperdiet sapien. Integer elementum et mauris
+      quis semper. Nulla facilisi. Morbi ut ultrices lorem. Cras interdum sem at
+      est tincidunt rhoncus non id lacus. Mauris facilisis at quam nec placerat.
+      Duis ut quam sed erat tincidunt pellentesque ac sed sapien. Nunc sit amet
+      risus sodales metus rutrum lobortis ut eget ex. Cras a diam sit amet erat
+      ultricies tempus. Pellentesque fermentum nisl eu odio iaculis, nec
+      pellentesque enim molestie. Fusce tempor nisi at scelerisque tempor. Nam
+      nec tempor arcu. Curabitur molestie eu nunc sit amet convallis. Duis
+      rutrum magna sit amet efficitur maximus. Sed interdum lectus dui, ac
+      lobortis tortor finibus eu. Sed congue sed justo non lobortis.
+    </Box>
+  );
+}
+
+export default Home;
