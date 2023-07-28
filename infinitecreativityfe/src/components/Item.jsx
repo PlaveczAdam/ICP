86619a49c52cea570affceb9b771fe3b78a3525d
Adam Plavecz <plaveczadam79@gmail.com>
2023-07-28 23:31:01 +0200

    change picture add tooltip to inventory

--- infinitecreativityfe/src/components/Item.jsx
+++ infinitecreativityfe/src/components/Item.jsx
@@ -1,10 +1,24 @@
-import { Box } from '@mui/material';
+import { Box, Tooltip } from "@mui/material";
+import {itemTypeName} from "../utils/OptionNames";
 
-function Item(props)
-{
-    return <Box width="50px" height="50px">
-        {props.item.id}
+function Item(props) {
+  const tooltipContent = (
+    <Box>
+      <Box display="grid" gridTemplateColumns="max-content 100px" gap={1}>
+        <Box>ItemName:</Box> <Box>{props.item.name}</Box>
+        <Box>Description:</Box> <Box>{props.item.description}</Box>
+        <Box>Price:</Box> <Box>{props.item.value}</Box>
+        <Box>ItemType:</Box> <Box>{itemTypeName[props.item.itemType]}</Box>
+      </Box>
     </Box>
+  );
+  return (
+    <Tooltip title={tooltipContent} arrow>
+      <Box width="50px" height="50px">
+        {props.item.id}
+      </Box>
+    </Tooltip>
+  );
 }
 
 export default Item;
---
+++ infinitecreativityfe/src/utils/OptionNames.js
@@ -0,0 +1,3 @@
+export const itemTypeName = {
+    0:"Weapon",
+};
