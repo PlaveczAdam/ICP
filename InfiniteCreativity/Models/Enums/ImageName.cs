8a624ee79e4fbf7fcd70e55d232554ee5512f9a4
Akos <barcza.akos15@gmail.com>
2023-07-31 22:30:54 +0200

    add placeholder image file names

--- InfiniteCreativity/Models/Enums/ImageName.cs
+++ InfiniteCreativity/Models/Enums/ImageName.cs
@@ -6,5 +6,11 @@
         public const string Bow = "bow.png";
         public const string Spear = "spear.png";
         public const string Armor = "armor.png";
+        public const string Head = "head.png";
+        public const string Leg = "leg.png";
+        public const string Boot = "boots.png";
+        public const string Hand = "hand.png";
+        public const string Shoulder = "shoulder.png";
+        public const string Chest = "chest.png";
     }
 }
