62787d52720716b6327d4ad1110b030c55665d51
Akos <barcza.akos15@gmail.com>
2023-08-02 18:15:28 +0200

    pass a true value to getcurrentplayer function

--- InfiniteCreativity/Services/PlayerService.cs
+++ InfiniteCreativity/Services/PlayerService.cs
@@ -86,7 +86,7 @@ namespace InfiniteCreativity.Services
 
         public async Task<ShowPlayerDTO> GetCurrentPlayerDTO()
         {
-            var player = await GetCurrentPlayer();
+            var player = await GetCurrentPlayer(true);
             return _mapper.Map<ShowPlayerDTO>(player);
         }
     }
