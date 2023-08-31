3c33c5d35733d6efde6bed9ced849ffe18346ce0
Adam Plavecz <plaveczadam79@gmail.com>
2023-08-31 18:59:26 +0200

    adjust mapgeneration parameters

--- InfiniteCreativity/Services/GameNS/MapGenerator.cs
+++ InfiniteCreativity/Services/GameNS/MapGenerator.cs
@@ -14,7 +14,7 @@ namespace Map
         public int rows = 25;
         public int columns = 25;
         private float landBias = 0.45f;
-        private float waterFrequency = 2f;
+        private float waterFrequency = 0.2f;
         private float treeToLandRatio = 0.25f;
         private int treeBatchMax = 10;
         private string mapOverride = "";
