ce3695953892993ddb13387ea1355085cc14e589
Adam Plavecz <plaveczadam79@gmail.com>
2023-09-01 20:13:55 +0200

    remvoe game connection from enemydto

--- InfiniteCreativity/DTO/Game/ShowGameEnemyDTO.cs
+++ InfiniteCreativity/DTO/Game/ShowGameEnemyDTO.cs
@@ -10,7 +10,6 @@ namespace InfiniteCreativity.DTO.Game
         public EnemyType EnemyType { get; set; }
 
         public double Health { get; set; }
-        public GConnection GConnection { get; set; }
         public string Name { get; set; }
         public virtual double MaxHealth { get; set; }
         public double Defense { get; set; }
