4843303bc133bda25d56594d5ca17233419540ab
Adam Plavecz <plaveczadam79@gmail.com>
2023-10-06 19:06:00 +0200

    fix showbattleparticipantdto

--- DTOs/Game/ShowBattleParticipantDTO.cs
+++ DTOs/Game/ShowBattleParticipantDTO.cs
@@ -13,6 +13,8 @@ namespace DTOs.Game
         public ShowGameCharacterDTO? Character { get; set; }
         public ShowGameEnemyDTO? Enemy { get; set; }
         public int Order { get; set; }
+        public int CurrentActionGauge { get; set; }
+        public int ActionGauge { get; set; }
         public double CurrentSpeed { get; set; }
 
     }
