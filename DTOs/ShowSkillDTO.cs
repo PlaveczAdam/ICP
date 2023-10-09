fa3fd43cc24c1cd992be7a0d4d247f7e59f34296
Adam Plavecz <plaveczadam79@gmail.com>
2023-10-09 18:31:56 +0200

    fix showskilldto

--- DTOs/ShowSkillDTO.cs
+++ DTOs/ShowSkillDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DTOs.Enums.GameNS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,6 @@ namespace DTOs
         public double ResourceCost { get; set; }
         public int AbilityGaugeCost { get; set; }
         public int Cooldown { get; set; }
+        public TargetType TargetType { get; set; }
     }
 }
