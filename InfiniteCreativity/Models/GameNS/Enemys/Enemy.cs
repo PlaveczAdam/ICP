28ac5a28ab64ed8083cf07972b5c39b8d031d233
Adam Plavecz <plaveczadam79@gmail.com>
2023-10-19 18:17:50 +0200

    fix enemy

--- InfiniteCreativity/Models/GameNS/Enemys/Enemy.cs
+++ InfiniteCreativity/Models/GameNS/Enemys/Enemy.cs
@@ -39,6 +39,8 @@ namespace InfiniteCreativity.Models.GameNS.Enemys
         public List<ShowBattleEventDTO> Turn(List<BattleParticipant> characterParticipants, BattleParticipant selfParticipant)
         {
             var result = new List<ShowBattleEventDTO>();
+            var modifiers = selfParticipant.CalculateStatModifications();
+
             while (selfParticipant.CurrentActionGauge > 0)
             {
                 var validTargets = characterParticipants
@@ -50,7 +52,7 @@ namespace InfiniteCreativity.Models.GameNS.Enemys
                 var target = validTargets
                     .MaxBy(x => x.Character!.Defense);
 
-                target!.Character!.TakeDamage(CalculateDamage());
+                target!.Character!.TakeDamage(CalculateDamage(modifiers));
 
                 result.Add(new ShowBattleEventEnemyAttackDTO()
                 {
@@ -74,12 +76,12 @@ namespace InfiniteCreativity.Models.GameNS.Enemys
 
             return result;
         }
-        public double CalculateDamage()
+        public double CalculateDamage(StatModifications modifications)
         {
             var critPower = _rnd.NextCrit(CriticalChance);
             var critMultiplier = Math.Pow(CriticalMultiplier, critPower);
 
-            return Damage * critMultiplier;
+            return Damage * critMultiplier * modifications.DamageMultiplier;
         }
 
         public void TakeDamage(double damage)
