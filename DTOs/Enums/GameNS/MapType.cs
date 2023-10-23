24bf30b68bd3e2c3ed1c4c37c9dfb0262b690e5b
Adam Plavecz <plaveczadam79@gmail.com>
2023-10-23 15:43:19 +0200

    add description to enum maptype

--- DTOs/Enums/GameNS/MapType.cs
+++ DTOs/Enums/GameNS/MapType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace DTOs.Enums.GameNS
 {
     public enum MapType
     {
+        [Description("Large")]
         Preset1,
+        [Description("Smol")]
         Preset2
     }
 }
