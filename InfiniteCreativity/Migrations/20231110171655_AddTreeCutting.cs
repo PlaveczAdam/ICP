3ad7ac0dd030a60c184b28c6f786ab764a6409ab
Adam Plavecz <plaveczadam79@gmail.com>
2023-11-10 18:17:19 +0100

    add migration tree cutting

---
+++ InfiniteCreativity/Migrations/20231110171655_AddTreeCutting.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfiniteCreativity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTreeCutting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TreeCuttingLeft",
+                table: "GameCharacter",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TreeCuttingLeft",
+                table: "GameCharacter");
+        }
+    }
+}
